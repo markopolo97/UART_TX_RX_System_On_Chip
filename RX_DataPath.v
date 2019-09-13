0c4a36596a7937e62083fcff7487ecfb031a18cb
Mark Aquiapao <52899299+markopolo97@users.noreply.github.com>
2019-09-12 22:39:28 -0700

    Update RX_DataPath.v

--- RX_DataPath.v
+++ RX_DataPath.v
@@ -13,22 +13,22 @@
  *
  ****************************************************************************/
 //*****************************************************************************
-module RX_DataPath(clk, rst, BTU, START, RX, EIGHT, PEN, OHEL, DONE, CLR,  
-						 REMAP_OUT, RXRDY, PERR, FERR, OVF);
+module RX_DataPath(	clk, rst, BTU, START, RX, EIGHT, PEN, OHEL, DONE, CLR,  
+			REMAP_OUT, RXRDY, PERR, FERR, OVF);
 //*****************************************************************************
 // INPUTS AND OUTPUTS:
 //*****************************************************************************				
-	input 				clk, rst, BTU, START, RX, EIGHT, PEN, OHEL, DONE, CLR;
-	output reg 			RXRDY, PERR, FERR, OVF;
+	input 		  clk, rst, BTU, START, RX, EIGHT, PEN, OHEL, DONE, CLR;
+	output reg 	  RXRDY, PERR, FERR, OVF;
 	output wire [7:0] REMAP_OUT;
 //*****************************************************************************
 // REG and WIRES
 //*****************************************************************************
-	reg					STOP_SEL;
-	reg			[9:0] SDO, REMAP;
-	wire					GEN_OUTPUT, P_GEN_BIT, EVEN,
-							GEN_SEL, BIT_SEL,
-							SET_PAR, SET_FRAM, SET_OVF;
+	reg		STOP_SEL;
+	reg	[9:0] 	SDO, REMAP;
+	wire		GEN_OUTPUT, P_GEN_BIT, EVEN,
+			GEN_SEL, BIT_SEL,
+			SET_PAR, SET_FRAM, SET_OVF;
 //*****************************************************************************
 // SHIFT REGISTER:
 //*****************************************************************************
@@ -36,7 +36,7 @@ module RX_DataPath(clk, rst, BTU, START, RX, EIGHT, PEN, OHEL, DONE, CLR,
 	always@(posedge clk, posedge rst)begin
 		if(rst)	SDO <= 10'b0;else
 		if(SH)	SDO <= {RX, SDO[9:1]}; // SHIFT in RX
-		else		SDO <= SDO; 
+		else	SDO <= SDO; 
 	end
 //*****************************************************************************
 // REMAP COMBO:
@@ -50,18 +50,17 @@ module RX_DataPath(clk, rst, BTU, START, RX, EIGHT, PEN, OHEL, DONE, CLR,
       endcase
 	end
 //*****************************************************************************
-	assign {REMAP_OUT, GEN_SEL, BIT_SEL} = (EIGHT) ? {REMAP[7:0], REMAP[7], REMAP[8]}:
-																	 {1'b0, REMAP[6:0], 1'b0, SDO[7]};	
-	assign EVEN	= ~OHEL;
+	assign {REMAP_OUT, GEN_SEL, BIT_SEL} = (EIGHT) ? {REMAP[7:0], REMAP[7], REMAP[8]}:															 {1'b0, REMAP[6:0], 1'b0, SDO[7]};	
+	assign EVEN	  = ~OHEL;
 	assign GEN_OUTPUT = (EVEN) ? (GEN_SEL ^ REMAP[6:0]): ~(GEN_SEL ^ REMAP[6:0]);
-	assign P_GEN_BIT 	= (GEN_OUTPUT ^ BIT_SEL);
+	assign P_GEN_BIT  = (GEN_OUTPUT ^ BIT_SEL);
 //----------------------------------------------------------------------
 	always@(*)begin
 		case({EIGHT, PEN})
 			2'b00 :	STOP_SEL = REMAP[7];
 			2'b01 :	STOP_SEL = REMAP[8];
 			2'b10 :	STOP_SEL = REMAP[9];
-			2'b11	:	STOP_SEL = REMAP[8];
+			2'b11 :	STOP_SEL = REMAP[8];
 		endcase
 	end
 //*****************************************************************************
@@ -71,40 +70,40 @@ module RX_DataPath(clk, rst, BTU, START, RX, EIGHT, PEN, OHEL, DONE, CLR,
 // RX READY:
 //----------------------------------------------------------------------------
 	always@(posedge clk, posedge rst)begin
-		if(rst)				RXRDY <= 1'b0;else		// Reset RXRDY
-		if(DONE)				RXRDY <= 1'b1;else		// Set 	RXRDY
-      if(CLR)				RXRDY <= 1'b0;			   // Clear RXRDY
-		else					RXRDY <= RXRDY;
+		if(rst)		RXRDY <= 1'b0;else		// Reset RXRDY
+		if(DONE)	RXRDY <= 1'b1;else		// Set 	RXRDY
+      		if(CLR)		RXRDY <= 1'b0;			// Clear RXRDY
+		else		RXRDY <= RXRDY;
 	end
 //----------------------------------------------------------------------------
 // Parity Error:
 //----------------------------------------------------------------------------
 	assign SET_PAR	= PEN  & P_GEN_BIT & DONE;
 	always@(posedge clk, posedge rst)begin
-		if(rst)				PERR <= 1'b0;else			// Reset PERR
-		if(SET_PAR)			PERR <= 1'b1;else			// Set 	PERR
-      if(CLR)				PERR <= 1'b0;			   // Clear PERR
-		else					PERR <= PERR;
+		if(rst)			PERR <= 1'b0;else	// Reset PERR
+		if(SET_PAR)		PERR <= 1'b1;else	// Set PERR
+      		if(CLR)			PERR <= 1'b0;		// Clear PERR
+		else			PERR <= PERR;
 	end
 //----------------------------------------------------------------------------
 // Framing Error:
 //----------------------------------------------------------------------------
 	assign SET_FRAM = DONE & ~STOP_SEL;
 	always@(posedge clk, posedge rst)begin
-		if(rst)				FERR <= 1'b0;else			// Reset FERR
-		if(SET_FRAM)		FERR <= 1'b1;else			// Set 	FERR
-      if(CLR)				FERR <= 1'b0;				// Clear FERR
-		else					FERR <= FERR;
+		if(rst)			FERR <= 1'b0;else	// Reset FERR
+		if(SET_FRAM)		FERR <= 1'b1;else	// Set 	FERR
+      		if(CLR)			FERR <= 1'b0;		// Clear FERR
+		else			FERR <= FERR;
 	end
 //----------------------------------------------------------------------------
 // Overflow:
 //----------------------------------------------------------------------------
 	assign SET_OVF	= DONE & RXRDY;
 	always@(posedge clk, posedge rst)begin
-		if(rst)				OVF <= 1'b0;else			// Reset OVF
-		if(SET_OVF)			OVF <= 1'b1;else			// Set 	OVF
-      if(CLR)				OVF <= 1'b0;				// Clear OVF
-		else					OVF <= OVF;
+		if(rst)			OVF <= 1'b0;else	// Reset OVF
+		if(SET_OVF)		OVF <= 1'b1;else	// Set 	OVF
+      		if(CLR)			OVF <= 1'b0;		// Clear OVF
+		else			OVF <= OVF;
 	end
 //*****************************************************************************
 endmodule
